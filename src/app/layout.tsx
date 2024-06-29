5a2b898b38f49588188a3dc7d58cc53e102d9c8b
theBadKing <tech.amf@gmail.com>
2024-06-29 21:02:43 +0100

    loading fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,16 +37,20 @@ export default function RootLayout({
 
   if (isLoading || !cpsrTheme) {
     return (
-      <div className="flex w-full items-center justify-center mt-44">
-        <div
-          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
-          role="status"
-        >
-          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-            Loading...
-          </span>
-        </div>
-      </div>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex w-full items-center justify-center mt-44">
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          </div>
+        </body>
+      </html>
     );
   }
 
