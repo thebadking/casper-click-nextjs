7dfb3b8afc06846410eb95d011d366fc7f73991c
theBadKing <tech.amf@gmail.com>
2024-07-29 00:47:07 +0100

    cleanup and update packages

--- src/hooks/useGetAuctionMetrics.tsx
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "react-query";
-import { getAuctionMetrics } from "../services/httpReq";
-import { AuctionMetricsPayload } from "../types";
-
-export const useGetAuctionMetrics = (intervalMs?: number) =>
-  useQuery<AuctionMetricsPayload, Error>("metrics-auction", getAuctionMetrics, {
-    refetchInterval: intervalMs,
-  });
--- src/hooks/useGetStatusInfos.tsx
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "react-query";
-import { getStatusInfos } from "../services/httpReq";
-import { StatusInfoPayload } from "../types";
-
-export const useGetStatusInfos = (intervalMs?: number) =>
-  useQuery<StatusInfoPayload, Error>("status", getStatusInfos, {
-    refetchInterval: intervalMs,
-  });
