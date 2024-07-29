de5e62cd40bc5a64658fa682c09c4aacd1fcc86c
theBadKing <tech.amf@gmail.com>
2024-07-29 01:13:30 +0100

    updated styles

--- src/components/GettingStarted/components/BuyMeACoffee.tsx
+++ src/components/GettingStarted/components/BuyMeACoffee.tsx
@@ -1,14 +1,41 @@
-import { useEffect, useState } from "react";
-import { Section } from "./Section";
-import { AccountIdenticon, useClickRef } from "@make-software/csprclick-ui";
-import { SendResult } from "@make-software/csprclick-core-client";
-import makeTransferDeploy from "./transfer-deploy";
-import Prism from "prismjs";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Section } from './Section';
+import { AccountIdenticon, useClickRef } from '@make-software/csprclick-ui';
+import { SendResult } from '@make-software/csprclick-core-client';
+import makeTransferDeploy from './TransferDeploy';
+import Prism from 'prismjs';
+
+export const StyledTD = styled.td(({ theme }) =>
+  theme.withMedia({
+    fontWeight: '600',
+    margin: '4px 15px 4px 0',
+    display: 'block'
+  })
+);
+
+export const SpanTruncated = styled.span(({ theme }) =>
+  theme.withMedia({
+    display: 'inline-block',
+    fontFamily: 'JetBrains Mono',
+    width: ['150px', '350px', '100%'],
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
+  })
+);
+const AccountRow = styled.div(({ theme }) =>
+  theme.withMedia({
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '8px',
+    alignItems: 'center'
+  })
+);
 
 export const BuyMeACoffee = () => {
   const [deployHash, setDeployHash] = useState<string | undefined>(undefined);
-  const recipientPk =
-    "0202dd8d878df6f2ecd0c9c012fd7cecc830f3623ecabeb914223c233c2c4046dcbb";
+  const recipientPk = '0202e5ea3177a56250a5eee5e0548d1507fdea86e239d4c61565e2800e820905d102';
 
   const clickRef = useClickRef();
   const activeAccount = clickRef?.getActiveAccount();
@@ -18,27 +45,22 @@ export const BuyMeACoffee = () => {
   }, []);
 
   const handleSignTransaction = async () => {
-    const sender = activeAccount?.public_key?.toLowerCase() || "";
-    const deploy = makeTransferDeploy(
-      sender,
-      recipientPk,
-      "50000000000",
-      "casper-test"
-    );
+    const sender = activeAccount?.public_key?.toLowerCase() || '';
+    const deploy = makeTransferDeploy(sender, recipientPk, '50' + '000000000', 'casper-test');
     clickRef
       ?.send(deploy, sender)
       .then((res: SendResult | undefined) => {
         if (res?.deployHash) {
           setDeployHash(res.deployHash);
-          alert("Transaction sent successfully: " + res.deployHash);
+          alert('Transaction sent successfully: ' + res.deployHash);
         } else if (res?.cancelled) {
-          alert("Sign cancelled");
+          alert('Sign cancelled');
         } else {
-          alert("Error in send(): " + res?.error + " - " + res?.errorData);
+          alert('Error in send(): ' + res?.error + '\n' + res?.errorData);
         }
       })
       .catch((err: any) => {
-        alert("Error: " + err);
+        alert('Error: ' + err);
         throw err;
       });
   };
@@ -47,35 +69,29 @@ export const BuyMeACoffee = () => {
     <>
       <Section>
         <span>
-          Your app will need to send transactions to Casper. Let&apos;s
-          illustrate how to do it buying a coffee for Alice with testnet CSPR
-          tokens.
+          Your app will need to send transactions to Casper. Let&apos;s illustrate how to do it
+          buying a coffee for Alice with testnet CSPR tokens.
         </span>
         <span>
-          First, build a transfer transaction deploy. The{" "}
-          <code>casper-js-sdk</code> is available in this template to do so.
-          Refer to the official{" "}
-          <a href={"https://casper-ecosystem.github.io/casper-js-sdk/"}>
-            SDK documentation
-          </a>{" "}
-          for more information and examples of usage.
+          First, build a transfer transaction deploy. The <code>casper-js-sdk</code> is available in
+          this template to do so. Refer to the official{' '}
+          <a href={'https://casper-ecosystem.github.io/casper-js-sdk/'}>SDK documentation</a> for
+          more information and examples of usage.
         </span>
         <span>
-          Next, call <code>clickRef.send()</code> method. CSPR.click will
-          request the user to sign the transaction in the active wallet and then
-          will send the transaction to a Casper node for processing it.
+          Next, call <code>clickRef.send()</code> method. CSPR.click will request the user to sign
+          the transaction in the active wallet and then will send the transaction to a Casper node
+          for processing it.
         </span>
         <span>
-          Notice in the example that your application must handle different
-          possible responses. Your app may show a success message with the
-          deploy hash when the transaction has been sent, but react
-          appropriately when the user rejects or the node reject the
-          transaction.
+          Notice in the example that your application must handle different possible responses. Your
+          app may show a success message with the deploy hash when the transaction has been sent,
+          but react appropriately when the user rejects or the node reject the transaction.
         </span>
       </Section>
       <Section>
         <pre>
-          <code className={"language-javascript"}>
+          <code className={'language-javascript'}>
             {`const handleSignTransaction = async () => {
   const sender = activeAccount?.public_key?.toLowerCase();
   const deploy = makeTransferDeploy(sender, recipientPk, '50000000000', 'casper-test');
@@ -94,54 +110,43 @@ export const BuyMeACoffee = () => {
           </code>
         </pre>
       </Section>
-      <Section>
-        <div className="overflow-hidden">
-          <table className="min-w-full  border-gray-200 shadow-sm rounded-lg">
-            <tbody>
-              <tr>
-                <td className="font-semibold px-4 py-2">Send:</td>
-                <td className="px-4 py-2">50 CSPR</td>
-              </tr>
-              <tr>
-                <td className="font-semibold px-4 py-2">From:</td>
-                <td className="px-4 py-2">
-                  <i>your account</i>
-                </td>
-              </tr>
-              <tr>
-                <td className="font-semibold px-4 py-2">To:</td>
-                <td className="px-4 py-2">
-                  <div className="flex gap-2 items-center">
-                    <AccountIdenticon hex={recipientPk} size="sm" />
-                    <span className="inline-block font-mono w-[150px] md:w-[350px] lg:w-full whitespace-nowrap overflow-hidden overflow-ellipsis">
-                      {recipientPk}
-                    </span>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={2} className="px-4 py-2">
-                  {activeAccount?.public_key && (
-                    <button
-                      className="bg-blue-500 hover:bg-blue-700  font-bold py-2 px-4 rounded"
-                      onClick={() => handleSignTransaction()}
-                    >
-                      Sign transaction
-                    </button>
-                  )}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <Section withBackground>
+        <table>
+          <tbody>
+            <tr>
+              <StyledTD>Send:</StyledTD>
+              <td>50 CSPR</td>
+            </tr>
+            <tr>
+              <StyledTD>From:</StyledTD>
+              <td>
+                <i>your account</i>
+              </td>
+            </tr>
+            <tr>
+              <StyledTD>To:</StyledTD>
+              <td>
+                <AccountRow>
+                  <AccountIdenticon hex={recipientPk} size="sm"></AccountIdenticon>
+                  <SpanTruncated>{recipientPk}</SpanTruncated>
+                </AccountRow>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                {activeAccount?.public_key && (
+                  <button onClick={() => handleSignTransaction()}>Sign transaction</button>
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
         {deployHash && (
           <a
             href={`${clickRef?.appSettings?.csprlive_url}deploy/${deployHash}`}
-            className="block mt-4 text-blue-500 hover:text-blue-700"
             target="_blank"
-            rel="noreferrer"
-          >
+            rel="noreferrer">
             Check transfer status on CSPR.live
           </a>
         )}
--- src/components/GettingStarted/index.tsx
+++ src/components/GettingStarted/index.tsx
@@ -1,20 +1,13 @@
-import React from "react";
-import {
-  Account,
-  BuyMeACoffee,
-  CustomMenuItems,
-  Section,
-  UseClickRefHook,
-} from "./components";
+import React from 'react';
+import { Account, BuyMeACoffee, CustomMenuItems, Section, UseClickRefHook } from './components';
+import Container from '../container';
 
 export const LandingBrief = () => {
   return (
-    <div>
+    <Container>
       <h3>✨ Multiple wallets integration</h3>
       <Section>
-        <span>
-          This project template comes with full support for multiple wallets:
-        </span>
+        <span>This project template comes with full support for multiple wallets:</span>
         <ul>
           <li>Casper Wallet</li>
           <li>Ledger</li>
@@ -24,15 +17,12 @@ export const LandingBrief = () => {
           <li>Casper Signer</li>
         </ul>
         <span>
-          Your app can interact with any of them using the same API. CSPR.click
-          takes care of everything wallet related. If needed, you can{" "}
+          Your app can interact with any of them using the same API. CSPR.click takes care of
+          everything wallet related. If needed, you can{' '}
           <a
-            href={
-              "https://docs.cspr.click/core-js-sdk/types#csprclickinitoptions"
-            }
-            target={"_blank"}
-            rel="noreferrer"
-          >
+            href={'https://docs.cspr.click/core-js-sdk/types#csprclickinitoptions'}
+            target={'_blank'}
+            rel="noreferrer">
             modify the list of enabled wallets
           </a>
           .
@@ -42,56 +32,50 @@ export const LandingBrief = () => {
       <h3>⚙️ Uniform UI/UX</h3>
       <Section>
         <span>
-          CSPR.click UI components provide a common UX across different Casper
-          apps for an easy management of accounts, settings, and future
-          ecosystem-wide functionality.
+          CSPR.click UI components provide a common UX across different Casper apps for an easy
+          management of accounts, settings, and future ecosystem-wide functionality.
         </span>
         <span>
           <a
-            href={"https://docs.cspr.click/ui-sdk/customizing-the-top-bar"}
-            target={"_blank"}
-            rel="noreferrer"
-          >
+            href={'https://docs.cspr.click/ui-sdk/customizing-the-top-bar'}
+            target={'_blank'}
+            rel="noreferrer">
             Customize the elements in the top bar
-          </a>{" "}
-          according to your application requirements. Add and remove settings
-          selectors.
+          </a>{' '}
+          according to your application requirements. Add and remove settings selectors.
         </span>
       </Section>
 
       <h3>🔝 Sign in</h3>
       <Section>
         <span>
-          Now, go back to the top of the page and sign in with your favorite
-          wallet. Or, click here:
+          Now, go back to the top of the page and sign in with your favorite wallet. Or, click here:
           <b
             onClick={(event) => {
               event.preventDefault();
               window.csprclick.signIn();
-            }}
-          >
-            {" "}
+            }}>
+            {' '}
             Connect
           </b>
           .
         </span>
       </Section>
-    </div>
+    </Container>
   );
 };
 
 export const SignedInBrief = () => {
   return (
-    <div>
+    <Container>
       <h3>🎉 Awesome! You have successfully signed in! What&#39;s next?</h3>
       <Section>
-        <span style={{ padding: "10px" }}>
-          Be sure that on this page you&#39;ll find all information you need for
-          future integration. Before you&apos;ll jump into development process,
-          please scroll down and get familiar with each section. Below
-          you&#39;ll find some useful code snipped with explanation and easy
-          integration examples about most important features in the SDK which
-          you&apos;ll use frequently.
+        <span style={{ padding: '10px' }}>
+          Be sure that on this page you&#39;ll find all information you need for future integration.
+          Before you&apos;ll jump into development process, please scroll down and get familiar with
+          each section. Below you&#39;ll find some useful code snipped with explanation and easy
+          integration examples about most important features in the SDK which you&apos;ll use
+          frequently.
         </span>
       </Section>
       <Section>
@@ -121,28 +105,26 @@ export const SignedInBrief = () => {
       <h3>🔥 Happy hacking!</h3>
       <Section>
         <span>
-          Finally! Time to focus on your new project! And, remember, you may
-          find guides and examples in{" "}
+          Finally! Time to focus on your new project! And, remember, you may find guides and
+          examples in{' '}
           <a
-            style={{ textDecoration: "underline" }}
-            href={"https://docs.cspr.click"}
-            target={"_blank"}
-            rel="noreferrer"
-          >
+            style={{ textDecoration: 'underline' }}
+            href={'https://docs.cspr.click'}
+            target={'_blank'}
+            rel="noreferrer">
             our documentation
           </a>
-          . Or you can reach to us on{" "}
+          . Or you can reach to us on{' '}
           <a
-            style={{ textDecoration: "underline" }}
-            href={"https://t.me/CSPRDevelopers"}
-            target={"_blank"}
-            rel="noreferrer"
-          >
+            style={{ textDecoration: 'underline' }}
+            href={'https://t.me/CSPRDevelopers'}
+            target={'_blank'}
+            rel="noreferrer">
             telegram
           </a>
           .
         </span>
       </Section>
-    </div>
+    </Container>
   );
 };
