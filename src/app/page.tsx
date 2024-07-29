a398ab1e5dec59e76c45c1c85626fbe412c6f2af
theBadKing <tech.amf@gmail.com>
2024-07-29 01:27:35 +0100

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,5 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { useClickRef } from '@make-software/csprclick-ui';
-import styled from 'styled-components';
-import Container from '@/components/container';
-import { Welcome } from '@/components/GettingStarted/components';
-import { LandingBrief, SignedInBrief } from '@/components/GettingStarted';
+import MainContainer from '@/components/CSPRMain';
 
-const GettingStartedContainer = styled.div(({ theme }) =>
-  theme.withMedia({
-    maxWidth: ['100%', '720px', '960px'],
-    padding: '0 12px',
-    margin: '0 auto'
-  })
-);
-
-const AppBody = () => {
-  const clickRef = useClickRef();
-  const [activeAccount, setActiveAccount] = useState(null);
-
-  useEffect(() => {
-    clickRef?.on('csprclick:signed_in', async (evt) => {
-      await setActiveAccount(evt.account);
-    });
-    clickRef?.on('csprclick:switched_account', async (evt) => {
-      await setActiveAccount(evt.account);
-    });
-    clickRef?.on('csprclick:signed_out', async () => {
-      setActiveAccount(null);
-    });
-    clickRef?.on('csprclick:disconnected', async () => {
-      setActiveAccount(null);
-    });
-  }, [clickRef?.on]);
-
-  return (
-    <Container>
-      <Welcome />
-      <GettingStartedContainer id={'getting-started'}>
-        {activeAccount ? <SignedInBrief /> : <LandingBrief />}
-      </GettingStartedContainer>
-    </Container>
-  );
-};
-
-export default AppBody;
+export default function Home() {
+  return <MainContainer />;
+}
